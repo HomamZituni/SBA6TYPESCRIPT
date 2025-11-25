6713b473d8f794e52e7f77032d5522a195c1c9dc
HomamZituni <homam93@msn.com>
2025-11-25 15:43:35 -0500

    added imports, async function and for each method to main.ts

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import {Product} from './models/Product';
+import {fetchProducts} from './services/apiService';
+import {calculateDiscount} from './utils/discountcalculator';
+import {calculateTax} from './utils/taxCalculator';
+import {handleError, CustomError} from './utils/errorHandler'
+
+async function main() {
+try {
+const productsData: await fetchProducts();
+const products: Product[] = productsData.map(p => new Product(p.id, p.title, p.price, p.category, p.discountPercentage));
+
+products.forEach(p => {
+p.displayDetails();
+console.log(`Price after discount: $${p.getPriceWithDiscount()}`);
+console.log(`Tax: $${calculateTax(p.price, p.category)}`);
+console.log('---------------------------');
+    });
+  } catch (error: any) {
+    handleError(error);
+  }
+}
+
+
+main(); 
