6521d149b625ad63d95ca90671586048f60891a7
HomamZituni <homam93@msn.com>
2025-11-25 23:06:41 -0500

    added export interface, and interface ProductsApi Response and added type assertion to json response to fix error

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,10 +1,24 @@
 import fetch from 'node-fetch';
 
+export interface ProductData {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  discountPercentage?: number;
+}
+
+interface ProductsApiResponse {
+  products: ProductData[];
+}
+
+
 export async function fetchProducts(): Promise <any[]> {
 try {
     const response = await fetch('https://dummyjson.com/products');
     if (!response.ok) throw new Error('HTTP error! Status: ${response.status}');
-    const data = await response.json();
+    const data = (await response.json()) as ProductsApiResponse;
+
     return data.products; 
     } catch (error) {
     console.error('Error fetching products:', error);
